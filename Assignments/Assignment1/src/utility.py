ea77cade6ca9ecd079f31682e11e885d788863fe
Hossein <m.h.askari.hemmat@gmail.com>
2018-02-05 23:52:28 -0500

    fixed bug for plotting dataset

--- Assignments/Assignment1/src/utility.py
+++ Assignments/Assignment1/src/utility.py
@@ -37,57 +37,46 @@ def load_dataset():
     train_sampler = SubsetRandomSampler(train_idx)
     valid_sampler = SubsetRandomSampler(valid_idx)
 
+    # Load to local variable
     train_loader = torch.utils.data.DataLoader(mnist_train, 
                     batch_size=batch_size, sampler=train_sampler, 
                     num_workers=1)
-
     valid_loader = torch.utils.data.DataLoader(mnist_valid, 
                     batch_size=batch_size, sampler=valid_sampler, 
                     num_workers=1)
-
-    # Load to local variable
-#    train_loader = torch.utils.data.DataLoader(
-#            mnist_train, batch_size=batch_size, shuffle=True, num_workers=1)
     test_loader = torch.utils.data.DataLoader(
             mnist_test, batch_size=batch_size, shuffle=True, num_workers=1)
 
-    sample_loader = torch.utils.data.DataLoader(mnist_train, 
-                                                    batch_size=9, 
-                                                    shuffle=True, 
-                                                    num_workers=1)
+    plot_sample_data(mnist_train)
+
+    return train_loader, test_loader
+
+def plot_sample_data(dataset, batch_size=9, plot_name="Title"):
+    sample_loader = torch.utils.data.DataLoader(dataset, 
+                                                batch_size=9, 
+                                                shuffle=True, 
+                                                num_workers=1)
     data_iter = iter(sample_loader)
     images, labels = data_iter.next()
     X = images.numpy()
     X = np.transpose(X, [0, 2, 3, 1])
-    plot_images(X, labels)
+    plot_images(X, labels, plot_name)
 
-    return train_loader, test_loader
 
-def plot_images(images, cls_true, cls_pred=None):
+def plot_images(images, labels, cls_pred=None):
 
-    assert len(images) == len(cls_true) == 9
+    assert len(images) == len(labels) == 9
 
     # Create figure with sub-plots.
     fig, axes = plt.subplots(3, 3)
 
     for i, ax in enumerate(axes.flat):
         # plot the image
-        ax.imshow(images[i, :, :, :], interpolation='spline16')
-        # get its equivalent class name
-        cls_true_name = label_names[cls_true[i]]
-            
-        if cls_pred is None:
-            xlabel = "{0} ({1})".format(cls_true_name, cls_true[i])
-        else:
-            cls_pred_name = label_names[cls_pred[i]]
-            xlabel = "True: {0}\nPred: {1}".format(cls_true_name, cls_pred_name)
-            
+        ax.imshow(images[i, :, :, 0], cmap='gray')
+        xlabel = "Pred: {0}".format(labels[i])
+        
         ax.set_xlabel(xlabel)
         ax.set_xticks([])
         ax.set_yticks([])
-
     plt.show()
 
-train_data, valid_data = load_dataset()
-print train_data.size()
-print valid_data.size()
