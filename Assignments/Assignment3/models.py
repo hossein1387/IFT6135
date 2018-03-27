3160dede33dc19461c6bc750c6ba8d2379e2581f
Hossein <m.h.askari.hemmat@gmail.com>
2018-03-27 01:56:28 -0400

    model generator from config

---
+++ Assignments/Assignment3/models.py
@@ -0,0 +1,76 @@
+import numpy as np
+import utility
+import config
+import matplotlib.pyplot as plt
+
+import os
+import os.path
+import time
+import sys
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.models as models
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision.datasets as datasets
+import torchvision.models as models
+import torch.utils.data.sampler as sampler
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torch.autograd as autograd
+import torch.distributions as distributions
+import ntm 
+from aio import EncapsulatedNTM
+
+class LSTM(nn.Module):
+    def __init__(self, config):
+        super(LSTM, self).__init__()
+        self.config = config
+
+        self.lstm=nn.LSTM(self.config['data_width']+1, self.config['num_hidden'])
+        self.mlp=nn.Linear(self.config['num_hidden'], self.config['data_width'])
+
+    def init_hidden(self, batch_size):
+        num_hidden = self.config['num_hidden']
+        self.hidden= ( autograd.Variable(torch.randn(1, batch_size, num_hidden)),\
+                  autograd.Variable(torch.randn((1, batch_size, num_hidden))) )
+    def forward(self, sequence):
+        # out, (self.hidden, self.cell) = self.lstm(inputs, None)
+        out, self.hidden = self.lstm(sequence, self.hidden)
+        out=self.mlp(out)
+        return out
+
+    def calculate_num_params(self):
+        """Returns the total number of parameters."""
+        num_params = 0
+        for p in self.parameters():
+            num_params += p.data.view(-1).size(0)
+        return num_params
+
+
+def build_model(config):
+    if config['model_type'] == 'LSTM':
+        model = LSTM(config)
+        criterion = nn.BCELoss()
+        optimizer = optim.Adam(model.parameters(),  lr=config['learning_rate'])
+    elif config['model_type'] == 'LSTM_NTM':
+        model = EncapsulatedNTM(num_inputs=config['data_width']+1, num_outputs=config['data_width']+1,\
+                                controller_size=100, controller_layers=1, num_heads=1, N=128, M=20, \
+                                controller_type ='lstm')
+        criterion = nn.BCELoss()
+        optimizer = optim.RMSprop(model.parameters(), lr=config['learning_rate'], momentum = config['momentum'])
+    elif config['model_type'] == 'MLP_NTM':
+        model = EncapsulatedNTM(num_inputs=config['data_width']+1, num_outputs=config['data_width'],\
+                                controller_size=100, controller_layers=1, num_heads=1, N=128, M=20, \
+                                controller_type ='mlp')
+        criterion = nn.BCELoss()
+        optimizer = optim.RMSprop(model.parameters(), lr=config['learning_rate'], momentum = config['momentum'])
+    else:
+        print ("Config type {0} is unknown".format(config['model_type']))
+        sys.exit()
+    return model, criterion, optimizer
