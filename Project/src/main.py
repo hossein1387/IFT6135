8e916f3e0e617d4f3ef88e28ac4acaf9e9c36cbe
Hossein <m.h.askari.hemmat@gmail.com>
2018-04-25 02:38:07 -0400

    ignoring MNIST downloaded dataset

---
+++ Project/src/main.py
@@ -0,0 +1,65 @@
+import torch 
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+import ipdb as pdb
+import utility
+import config
+import models 
+
+
+def build_model(config):
+    model = models.CNN()
+    # Loss and Optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"])
+    return model, criterion, optimizer
+
+def test_model(test_loader):
+    # Test the Model
+    model.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = Variable(images)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+
+    print('Test Accuracy of the model on the 10000 test images: %d %%' % (100 * correct / total))
+
+
+def train_model(model, criterion, optimizer, train_loader, train_dataset, config):
+    # Train the Model
+    num_epochs = config['num_epochs']
+    batch_size = config['batchsize']
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            images = Variable(images)
+            labels = Variable(labels)
+            
+            # Forward + Backward + Optimize
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            if (i+1) % 100 == 0:
+                print ('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f' 
+                       %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data[0]))
+
+
+if __name__ == '__main__':
+    args = utility.parse_args()
+    config_type = args['configtype']
+    config_file = args['configfile']
+    config = config.Configuration(config_type, config_file)
+    print(config.get_config_str())
+    config = config.config_dict
+    model, criterion, optimizer = build_model(config)
+    train_loader, test_loader, train_dataset, test_dataset = utility.load_dataset(config)
+    train_model(model, criterion, optimizer, train_loader, train_dataset, config)
+    test_model(test_loader)
+    # Save the Trained Model
+    torch.save(model.state_dict(), 'trained_model.pkl')
---
+++ Project/src/utility.py
@@ -0,0 +1,42 @@
+import argparse
+import os
+import random
+import sys
+import torchvision.datasets as dsets
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import torch
+import torch.nn.parallel
+import yaml
+from torch.autograd import Variable
+import torchvision.transforms as transforms
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--configfile', help='config file in yaml format', required=True)
+    parser.add_argument('-t', '--configtype', help='configurations to run', required=True)
+    args = parser.parse_args()
+    return vars(args)
+
+def load_dataset(config):
+    # MNIST Dataset
+    train_dataset = dsets.MNIST(root='./data/',
+                                train=True, 
+                                transform=transforms.ToTensor(),
+                                download=True)
+
+    test_dataset = dsets.MNIST(root='./data/',
+                               train=False, 
+                               transform=transforms.ToTensor())
+
+    # Data Loader (Input Pipeline)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=config["batchsize"], 
+                                               shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=config["batchsize"], 
+                                              shuffle=False)
+    return train_loader, test_loader, train_dataset, test_dataset
