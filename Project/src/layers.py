8ec08a68b0cf02606c6d0a8e69ce4765ae484f70
Hossein <m.h.askari.hemmat@gmail.com>
2018-04-30 01:08:19 -0400

    adding conv layer for a wage model

--- Project/src/layers.py
+++ Project/src/layers.py
@@ -120,3 +120,59 @@ class SignBNN(torch.nn.Module):
         super().__init__()
     def forward(self, x):
         return bnn_sign(x)
+
+
+#
+# WAGE CNN
+#
+
+class Conv2dWAGE(torch.nn.Conv2d):
+    """
+    Convolution layer for BinaryNet.
+    """
+    
+    def __init__(self, in_channels,
+                       out_channels,
+                       kernel_size,
+                       stride       = 1,
+                       padding      = 0,
+                       dilation     = 1,
+                       groups       = 1,
+                       bias         = True,
+                       H            = 1.0,
+                       W_LR_scale   = "Glorot"):
+        #
+        # Fan-in/fan-out computation
+        #
+        num_inputs = in_channels
+        num_units  = out_channels
+        for x in kernel_size:
+            num_inputs *= x
+            num_units  *= x
+        
+        if H == "Glorot":
+            self.H          = float(np.sqrt(1.5/(num_inputs + num_units)))
+        else:
+            self.H          = H
+        
+        if W_LR_scale == "Glorot":
+            self.W_LR_scale = float(np.sqrt(1.5/(num_inputs + num_units)))
+        else:
+            self.W_LR_scale = self.H
+        
+        super().__init__(in_channels, out_channels, kernel_size,
+                         stride, padding, dilation, groups, bias)
+        self.reset_parameters()
+    
+    def reset_parameters(self):
+        self.weight.data.uniform_(-self.H, +self.H)
+        if isinstance(self.bias, torch.nn.Parameter):
+            self.bias.data.zero_()
+    
+    def constrain(self):
+        self.weight.data.clamp_(-self.H, +self.H)
+    
+    def forward(self, x):
+        W = Quantize.W(self.W[-1], self.config['wbits'])
+        return TNF.conv2d(x, Wb, self.bias, self.stride, self.padding, self.dilation, self.groups)
+
